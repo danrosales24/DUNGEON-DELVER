c89a7919ca6946e28dde3eff6ce7d54b7c939059
danrosales24 <52152236+danrosales24@users.noreply.github.com>
2019-12-06 19:58:37 -0500

    tiltes in

---
+++ Assets/__Scripts/Tile.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+
+    [Header("Set Dynamically")]
+    public int x;
+    public int y;
+    public int titleNum;
+
+    public void SetTitle(int eX, int eY, int EtileNum = -1)
+    {
+        x = eX;
+        y = eY;
+        transform.localPosition = new Vector3(x, y, 0);
+        gameObject.name =x.ToString("D3")+"+y.ToString("D3");
+    }
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
