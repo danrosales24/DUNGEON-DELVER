87a2ae3c21434eb99b3277832eeba9847883772c
danrosales24 <52152236+danrosales24@users.noreply.github.com>
2019-12-16 01:04:05 -0500

    bug fix tiltecam error redownloaded finish verison

--- Assets/__Scripts/Tile.cs
+++ Assets/__Scripts/Tile.cs
@@ -1,12 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 public class Tile : MonoBehaviour
 {
+
     [Header("Set Dynamically")]
     public int x;
     public int y;
     public int tileNum;
+
+    private BoxCollider bColl;
+
+    private void Awake()
+    {
+        bColl = GetComponent<BoxCollider>();
+    }
+
     public void SetTile(int eX, int eY, int eTileNum = -1)
     {
         x = eX;
@@ -17,8 +27,42 @@ public class Tile : MonoBehaviour
         {
             eTileNum = TileCamera.GET_MAP(x, y);
         }
+        else
+        {
+            TileCamera.SET_MAP(x, y, eTileNum);
+        }
         tileNum = eTileNum;
-        GetComponent<SpriteRenderer>().sprite =
-        TileCamera.SPRITES[tileNum]; // d
+        GetComponent<SpriteRenderer>().sprite = TileCamera.SPRITES[tileNum];
+        SetCollider();
     }
+
+    void SetCollider()
+    {
+        bColl.enabled = true;
+        char c = TileCamera.COLLISIONS[tileNum];
+        switch (c)
+        {
+            case 'S':
+                bColl.center = Vector3.zero;
+                bColl.size = Vector3.one;
+                break;
+            case 'W':
+                bColl.center = new Vector3(0, 0.25f, 0);
+                bColl.size = new Vector3(1, 0.5f, 1);
+                break;
+            case 'A':
+                bColl.center = new Vector3(-0.25f, 0, 0);
+                bColl.size = new Vector3(0.5f, 1, 1);
+                break;
+            case 'D':
+                bColl.center = new Vector3(.25f, 0 , 0);
+                bColl.size = new Vector3(.5f, 1, 1);
+                break;
+            default:
+                bColl.enabled = false;
+                break;
+        }
+    }
+
+
 }
